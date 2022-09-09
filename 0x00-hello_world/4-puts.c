4ba8aff15bd604c250d9eff7d4a93e7a8969e1c9
laplaceAA <vtakintola@gmail.com>
2022-09-08 17:09:57 -0700

    its C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * int main -  print an output 
- * return: 0
-  */
+ * main -  Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

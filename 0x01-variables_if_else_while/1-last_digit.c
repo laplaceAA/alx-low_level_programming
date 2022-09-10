25e7ab9d0c586410ffc26d3ead71e3c242c98518
laplaceAA <vtakintola@gmail.com>
2022-09-10 13:16:40 -0700

    into c proper

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,13 @@ int main(void)
 	{
 		printf("%d and is 0\n", n%10);
 	}
-	else if (n%10 < 6 && n%10 != 0);
+	else if (n%10 < 6 && n%10 != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", n%10);
+	}
+	else 
+	{
+
 	}
 
 	return (0);
